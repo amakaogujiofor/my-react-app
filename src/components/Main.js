3d2e38c299164b6d8686c6a641c6174391e3c979
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-17 19:27:22 +0100

    Modified and added new content to list

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Main() {
-  return <h1>Main Content component</h1>;
+  return (
+    <div className="container">
+      <h1 className="title-text">Fun facts about React </h1>
+      <ul className="main-content">
+        <li>Was first released in 2013 </li>
+        <li>Was originally created by Jordan Walke </li>
+        <li>Has well over 100k stars on Github </li>
+        <li>Is maintained by Facebook </li>
+        <li>Powers thousands of enterprise apps including mobile apps </li>
+      </ul>
+    </div>
+  );
 }
